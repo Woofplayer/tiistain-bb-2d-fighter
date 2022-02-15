337c75ccfd06f60d2ab890aedf16dec95f440dd9
vaksis <54946934+asecoder3@users.noreply.github.com>
2022-02-15 19:25:29 +0200

    liikkuminen, hyppy ja idle

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Rigidbody2D = myRigidBody;
+
+    private Rigidbody2D myRigidBody;
+    private CircleCollider2D myFeet;
+
+    private LayerMask ground;
+
+    private float speed = 5f;
+    private float horizontalMovement = 0f;
+    private float jumpForce = 7f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<CircleCollider2D>();
+        ground = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && myFeet.IsTouchingLayers(ground))
+        {
+            myRigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(horizontalMovement * speed, myRigidBody.velocity.y);
     }
 }
